a1c1c5aec46687d8db66ea2616c0184467815c14
shanu shahbin <shanushahbin11@gmail.com>
2023-08-07 10:26:26 +0530

    pop up created

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+tim = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="make your bet", prompt="which turtle will win the race? enter a color: ")
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
