b770ec83818787d7d50755e945021c31ad8a15af
shanu shahbin <shanushahbin11@gmail.com>
2023-08-07 10:57:38 +0530

    function created

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from turtle import Turtle, Screen
-tim = Turtle()
+
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="make your bet", prompt="which turtle will win the race? enter a color: ")
-
-
+colours = ["red", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, -30, -60]
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colours[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 
 
